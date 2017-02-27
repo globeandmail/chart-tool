beada238ce63dc69a71b8b0d779a8ec6ecabc0db
lexieheinle <lexie.heinle@gmail.com>
2017-02-27 11:18:26 -0500

    Pushing out correct build

--- meteor/lib/chart-tool/scripts/settings.js
+++ meteor/lib/chart-tool/scripts/settings.js
@@ -19,8 +19,8 @@ var bands = {"padding":0.12,"offset":0.06,"outerPadding":0.06};
 var source = {"prefix":"The Buffalo News","suffix":" » SOURCE:"};
 var social = {"facebook":{"label":"Facebook","icon":"https://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/svgs/fi-social-facebook.svg","redirect":"","appID":""},"twitter":{"label":"Twitter","icon":"https://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/svgs/fi-social-twitter.svg","via":"","hashtag":""},"email":{"label":"Email","icon":"https://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/svgs/fi-mail.svg"},"sms":{"label":"SMS","icon":"https://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/svgs/fi-telephone.svg"}};
 var image = {"enable":false,"base_path":"charts/thumbnails","expiration":30000,"filename":"thumbnail","extension":"png","thumbnailWidth":460};
-var embedJS = "http://beta.images.theglobeandmail.com/static/templates/tools/chart-tool/1.2.0/chart-tool.min.js";
-var embedCSS = "http://beta.images.theglobeandmail.com/static/templates/tools/chart-tool/1.2.0/chart-tool.min.css";
+var embedJS = "http://projects.buffalonews.com/charts/js/chart-tool.min.js";
+var embedCSS = "http://projects.buffalonews.com/charts/css/chart-tool.min.css";
 var config = {
 	CUSTOM: CUSTOM,
 	prefix: prefix$1,
