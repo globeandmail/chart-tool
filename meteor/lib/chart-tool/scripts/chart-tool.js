bd13382b81c75b5c01564810d28658ead5941719
Tom Cardoso <tcardoso@globeandmail.com>
2017-11-10 17:51:14 -0500

    version bump

--- meteor/lib/chart-tool/scripts/chart-tool.js
+++ meteor/lib/chart-tool/scripts/chart-tool.js
@@ -1,4 +1,4 @@
-/* Chart Tool v1.2.2-0 | https://github.com/globeandmail/chart-tool | MIT */
+/* Chart Tool v1.2.3-0 | https://github.com/globeandmail/chart-tool | MIT */
 var ChartToolInit = (function () {
 'use strict';
 
@@ -4157,7 +4157,7 @@ var bands = {"padding":0.12,"offset":0.06,"outerPadding":0.06};
 var social = {"facebook":{"label":"Facebook","icon":"https://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/svgs/fi-social-facebook.svg","redirect":"","appID":""},"twitter":{"label":"Twitter","icon":"https://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/svgs/fi-social-twitter.svg","via":"","hashtag":""},"email":{"label":"Email","icon":"https://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/svgs/fi-mail.svg"},"sms":{"label":"SMS","icon":"https://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/svgs/fi-telephone.svg"}};
 var image = {"enable":false,"base_path":"","expiration":30000,"filename":"thumbnail","extension":"png","thumbnailWidth":460};
 
-var version = "1.2.2";
+var version = "1.2.3";
 var buildVer = "0";
 
 var chartSettings = {
--- meteor/lib/chart-tool/scripts/settings.js
+++ meteor/lib/chart-tool/scripts/settings.js
@@ -48,7 +48,7 @@ var config = {
 };
 
 var name = "chart-tool";
-var version = "1.2.2";
+var version = "1.2.3";
 var buildVer = "0";
 
 app_version = version;
