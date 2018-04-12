d83819a45501094d293361efdc73b58bbf9c611b
Tom Cardoso <tcardoso@globeandmail.com>
2018-04-11 22:33:26 -0400

    updates heroku script

--- heroku.sh
+++ heroku.sh
@@ -15,16 +15,13 @@ git commit -m "Updating buildpacks, removing gitignore"
 
 git filter-branch -f --prune-empty --subdirectory-filter meteor "deploy-${CURRHASH}"
 
-heroku addons:create mongolab:sandbox
+# heroku addons:create mongolab:sandbox
 heroku config:add MONGO_URL=$DEMO_CHARTTOL_MONGO
 heroku labs:enable http-session-affinity
 heroku config:add ROOT_URL=http://chart-tool-demo.herokuapp.com
 
-heroku buildpacks:set 'https://github.com/heroku/heroku-buildpack-multi.git'
-
 echo -n "" > .buildpacks
 
-echo 'https://github.com/dscout/wkhtmltopdf-buildpack.git' >> .buildpacks
 echo 'https://github.com/tomcardoso/heroku-buildpack-meteor.git' >> .buildpacks
 
 git add .
